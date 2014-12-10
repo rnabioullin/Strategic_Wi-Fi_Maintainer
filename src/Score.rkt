e4a9d21ece2a9ccba3c37dbce47ac0afa8bf85b3
Tylor Reeves <tylor_reeves@student.uml.edu>
2014-12-10 04:00:21 -0500

    Updated definitions

--- src/Score.rkt
+++ src/Score.rkt
@@ -1,16 +1,22 @@
-#lang racket
+;#lang racket
 
 (define ssid-db '())
 
+;;Adds a list containing the attributes of a network to the network db
 (define (add-to-list ssid prevsuccess)
   (set! ssid-db (append ssid-db (list (list ssid prevsuccess (current-inexact-milliseconds))))))
 
+;;Checks the network db for a network based on ssid, and
+;;Returns an appropriate status based on whether or not
+;;The network was previously connected to successfully
 (define (check-for-network ssid db)
   (cond ((null? db) "No Previous Connection Attempts")
         ((and (equal? ssid (car (car db))) (equal? (car (cdr (car db))) #f)) "Bad Connection")
         ((and (equal? ssid (car (car db))) (equal? (car (cdr (car db))) #t)) "Good Connection")
         (else (check-for-network ssid (cdr db)))))
 
+;;Removes all network entries from the database that are older than
+;;a given time (in milliseconds) "network-age"
 (define (prune-db network-age)
   (define (prune-iter db newdb)
     (cond ((null? db) newdb)
@@ -18,29 +24,3 @@
          (prune-iter (cdr db) (append newdb (list (car db)))))
         (else (prune-iter (cdr db) newdb))))
   (set! ssid-db (prune-iter ssid-db '())))
-
-;(define (prune-db)
- ; (map (lambda (x) (< (current-inexact-milliseconds) (- (car (cdr (cdr x))) 25000))) ssid-db))
-  
-;(define (score-con ssid key quality prevsuccess)
-;  (cond ((eq? #t prevsuccess) (+ 2 quality))
-;        ((eq? #t key) quality)
-;        (else (+ 1 quality))))
-
-;(define (list-ssid ssid prevsuccess)
-;  (list ssid prevsuccess)
-
-;(define (add-to-list ssid key quality prevsuccess)
-;  (set! ssid-db (append ssid-db (list (list-ssid ssid key quality prevsuccess))))
-;  (set! ssid-db (sort ssid-db #:key cadr >)))
-
-;(define (get-best-ssid)
-;retrieves connection with highest score; deletes connection if connection attempt fails
-;  (cond ((null? ssid-db) (display "No Connections Available"))
-;    ((equal? "Connection Failed" (test-connect (car ssid-db)))
-;       (set! ssid-db (cdr ssid-db))
-;       (get-best-ssid))
-;    (else (car ssid-db))))
-
-;(define (test-connect ssid)
-;  1)
